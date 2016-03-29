dd0a0dbc48938f18b78eb157d82f7b09f17499ae
asouza <alots.ssa@gmail.com>
2016-03-29 18:56:23 -0300

    max age

--- src/main/java/br/com/caelum/fj91/performance/controllers/TransacoesController.java
+++ src/main/java/br/com/caelum/fj91/performance/controllers/TransacoesController.java
@@ -3,7 +3,10 @@ package br.com.caelum.fj91.performance.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
@@ -29,22 +32,28 @@ public class TransacoesController {
 	}
 
 	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/transacoes/paginada")
-	@ResponseBody
 	@Cacheable(value = "transacoes")
 	@Async
-	public Iterable<Transacao> lista(int pagina, int size) {
+	public ResponseEntity<Iterable<Transacao>> lista(int pagina, int size) {
 		System.out.println("chegou aqui..");
-		return transacaoDao.findAll(new PageRequest(pagina, size));
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("max-age", "36000");
+		ResponseEntity<Iterable<Transacao>> response = new ResponseEntity<Iterable<Transacao>>(
+				transacaoDao.findAll(new PageRequest(pagina, size)),
+				headers,
+				HttpStatus.OK);
+		
+		return response;
 	}
 
-	//aqui ta com get so para facilitar o teste pelo navegador
+	// aqui ta com get so para facilitar o teste pelo navegador
 	@RequestMapping(value = "/nova/transacao")
 	@ResponseBody
 	public String salva(Transacao transacao) throws InterruptedException {
-		jmsTemplate.send("novas-transacoes", (session) -> {			
+		jmsTemplate.send("novas-transacoes", (session) -> {
 			return session.createObjectMessage(transacao);
-		});				
-		return  "transacao salva";
+		});
+		return "transacao salva";
 	}
 
 }
